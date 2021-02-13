4ec7894507bde9a05c6fead50533de948837162c
alanh <alanh@192.168.1.52>
2021-02-13 11:31:19 -0600

    WINDOW

---
+++ src/gui/Main.java
@@ -0,0 +1,31 @@
+
+package gui;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class Main {
+    
+    public static void main (String[] args){
+        
+        JFrame frame = new JFrame();
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+        frame.setTitle("Cesba Ventana");
+        
+        frame.setResizable(true);
+        
+        //FORMATO DEFAULT
+        //frame.getContentPane().setBackground(Color.BLACK);
+        
+        //FORMATO RGB
+        //frame.getContentPane().setBackground(new Color(28, 26, 198));
+        
+        //FORMATO HEXADECIMAL
+        frame.getContentPane().setBackground(new Color(0x1AC3C6));
+        
+        
+    } 
+            
+            
+}
